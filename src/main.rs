b53a3c2bb70940acdcb2546dd8061fb96ded5731
Protoculos <mazdak2012@gmail.com>
2023-12-06 22:05:54 +0300

    added scroll button

--- src/main.rs
+++ src/main.rs
@@ -540,5 +540,17 @@ fn App(cx: Scope) -> Element {
                 }
             }
         }
+        // Scroll
+        a {
+            // Scroll
+            class: "fixed right-4 bottom-4 h-11 w-11 bg-secondaryColor shadow-sm flex rounded-full text-lg text-blackColor z-50 hover:-translate-y-1 ease-in duration-200 items-center justify-center",
+            href: "#",
+            svg {
+                class: "fill-current h-6 w-6 text-blackColor",
+                xmlns: "http://www.w3.org/2000/svg",
+                view_box: "0 0 384 512",
+                path { d: "M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" }
+            }
+        }
     }
 }
